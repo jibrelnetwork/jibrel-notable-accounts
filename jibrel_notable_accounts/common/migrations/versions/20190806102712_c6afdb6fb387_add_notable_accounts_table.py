a2bec5acaa314a8b57b76bcf8c15a7b487f32fd4
Nick Gashkov <nick@gashkov.com>
2019-08-06 13:28:01 +0300

    JSEARCH-466: Add notable accounts table

---
+++ jibrel_notable_accounts/common/migrations/versions/20190806102712_c6afdb6fb387_add_notable_accounts_table.py
@@ -0,0 +1,38 @@
+"""Add notable accounts table
+
+Revision ID: c6afdb6fb387
+Revises: 
+Create Date: 2019-08-06 10:27:12.315967
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'c6afdb6fb387'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+UP_SQL = """
+CREATE TABLE notable_accounts (
+    address CHARACTER VARYING NOT NULL,
+    name CHARACTER VARYING NOT NULL,
+    labels CHARACTER VARYING[] NOT NULL
+);
+
+ALTER TABLE ONLY notable_accounts
+    ADD CONSTRAINT notable_accounts_pkey PRIMARY KEY (address);
+"""
+
+DOWN_SQL = """
+DROP TABLE notable_accounts;
+"""
+
+
+def upgrade():
+    op.execute(UP_SQL)
+
+
+def downgrade():
+    op.execute(DOWN_SQL)
