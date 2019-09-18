b39738d669333a5b0fd532c90191d1c07e2bc065
Nick Gashkov <nick@gashkov.com>
2019-09-18 17:15:34 +0300

    ETHBE-577: Move 'NotableAccountView' into separate module

---
+++ jibrel_notable_accounts/admin/model_views.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from flask_admin.actions import action
+from flask_admin.contrib.sqla import ModelView
+
+from jibrel_notable_accounts.admin.app import db
+from jibrel_notable_accounts.admin.database_queries import update_is_admin_reviewed_true, update_is_admin_reviewed_false
+from jibrel_notable_accounts.common.tables import notable_accounts_t
+
+
+class NotableAccount(db.Model):  # type: ignore
+    __table__ = notable_accounts_t
+
+
+class NotableAccountView(ModelView):
+    column_display_pk = True
+    column_searchable_list = ('address', 'name')
+    column_sortable_list = ('address', 'name', 'labels', 'is_admin_reviewed')
+
+    column_default_sort = [('name', True), ('address', True)]
+    column_filters = ('address', 'name', 'is_admin_reviewed')
+
+    can_set_page_size = True
+
+    form_columns = ('address', 'name', 'labels', 'is_admin_reviewed')
+
+    @action('review', 'Review', 'Are you sure you want to review selected accounts?')
+    def review_accounts(self, ids: List[str]):
+        with db.engine.connect() as conn:
+            conn.execute(update_is_admin_reviewed_true(ids))
+
+    @action('unreview', 'Unreview', 'Are you sure you want to unreview selected accounts?')
+    def unreview_accounts(self, ids: List[str]):
+        with db.engine.connect() as conn:
+            conn.execute(update_is_admin_reviewed_false(ids))
+
+
+notable_account_view = NotableAccountView(NotableAccount, db.session)
