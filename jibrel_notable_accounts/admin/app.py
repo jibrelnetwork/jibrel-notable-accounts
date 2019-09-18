dad454e0fa37e928dc5587dd7c59706b1d368fa6
Nick Gashkov <nick@gashkov.com>
2019-09-18 15:23:42 +0300

    ETHBE-576: Integrate 'Flask-BasicAuth'

--- jibrel_notable_accounts/admin/app.py
+++ jibrel_notable_accounts/admin/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_basicauth import BasicAuth
 from flask_sqlalchemy import SQLAlchemy
 
 from jibrel_notable_accounts import settings
@@ -9,9 +10,14 @@ from jibrel_notable_accounts.common.tables import notable_accounts_t
 app = Flask(__name__)
 app.secret_key = settings.ADMIN_SECRET_KEY
 
+app.config['BASIC_AUTH_FORCE'] = settings.ADMIN_BASIC_AUTH_FORCE
+app.config['BASIC_AUTH_USERNAME'] = settings.ADMIN_BASIC_AUTH_USERNAME
+app.config['BASIC_AUTH_PASSWORD'] = settings.ADMIN_BASIC_AUTH_PASSWORD
+
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.DB_DSN
 app.config['FLASK_ADMIN_SWATCH'] = settings.ADMIN_UI_THEME
 
+basic_auth = BasicAuth(app)
 db = SQLAlchemy(app)
 
 
